0c1df307981694a4dd5620be7b52ee6833cf0678
Javier Conde <javo.edu83@gmail.com>
2021-06-05 10:26:16 -0500

    ejercicios

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //EJERCIOS DE ALGORITMOS
 
-//1
+/*
+1 SUMA DE 2 NUMEROS
 const numero0 = 2,
       numero1 = 3;
 
@@ -8,7 +9,7 @@ let resultado;
 resultado = numero0 + numero1;
 console.log(resultado);
 
-//2
+//2 PROMEDIO DE 4 NOTAS
 const nota1 = 15,
       nota2 = 15,
       nota3 = 14,
@@ -19,5 +20,43 @@ const suma4notas = nota1 + nota2 + nota3 + nota4;
 const Promedio = suma4notas / dividir;
 
 console.log(`
-El promedio de los 4 examenes es ${Promedio}
+El promedio de los 4 exámenes es ${Promedio}
 `);
+
+//3 AREA DEL RECTANGULO
+const base = 8,
+      altura = 3;
+const area = base * altura;
+
+console.log (`
+El área del rectangulo es ${area}
+`);
+
+*/
+
+//4  AREA DEL TRIANGULO
+const base = 8,
+      altura = 6;
+
+const sobre2 = 2;
+
+const BasePorAltura = base * altura;
+const area = BasePorAltura / sobre2;
+
+console.log(`
+El área del triágulo es ${area}
+`);
+
+//5 AREA DE LA CIRCUNFERENCIA
+
+const radio = 5,
+      pi = 3.14;
+
+const radioalcuadro = 5 * 5;
+const areacircunferencia = radioalcuadro * pi;
+
+console.log(`
+El área de la circunferencia es ${areacircunferencia}
+`);
+
+//6 
