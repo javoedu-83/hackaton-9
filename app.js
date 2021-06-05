6ca6cffd29bb646395caf1ab7c4f35b922b3dd0a
Javier Conde <javo.edu83@gmail.com>
2021-06-05 09:41:16 -0500

    resolviendo1

--- app.js
+++ app.js
@@ -7,3 +7,17 @@ const numero0 = 2,
 let resultado;
 resultado = numero0 + numero1;
 console.log(resultado);
+
+//2
+const nota1 = 15,
+      nota2 = 15,
+      nota3 = 14,
+      nota4 = 10;
+
+const dividir = 4;
+const suma4notas = nota1 + nota2 + nota3 + nota4;
+const Promedio = suma4notas / dividir;
+
+console.log(`
+El promedio de los 4 examenes es ${Promedio}
+`);
